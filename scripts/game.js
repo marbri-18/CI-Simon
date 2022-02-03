266182d8e187d4670846552d6b5f8724824c0538
Martin Bridgman <martin.bridgman@gmail.com>
2022-02-03 02:45:53 +0000

    Testing our first function Part 1

---
+++ scripts/game.js
@@ -0,0 +1,13 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMove: [],
+    choices: [],
+}
+
+
+
+
+
+
+module.exports = { game };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,10 +1,28 @@
 /**
  * @jest-environment jsdom
  */
-beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
+
+const { game } = require("../game")
+
+ beforeAll(() => {
+     let fs = require("fs");
+     let fileContents = fs.readFileSync("index.html", "utf-8");
+     document.open();
+     document.write(fileContents);
+     document.close();
+ });
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMove key exists", () => {
+        expect("playerMove" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
 });
